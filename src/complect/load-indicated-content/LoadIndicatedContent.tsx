f86be5b9dd3ff73c891c05990cc0ae10037233f0
PSlobodskoy <PSlobodskoy@one.kz>
2022-04-17 15:54:32 +0300

    loading-content fixed

--- src/complect/load-indicated-content/LoadIndicatedContent.tsx
+++ src/complect/load-indicated-content/LoadIndicatedContent.tsx
@@ -1,66 +1,65 @@
-import { PropsWithChildren, useEffect, useMemo, useRef, useState } from "react";
+import { PropsWithChildren, useEffect, useMemo, useState } from "react";
 import "./LoadIndicatedContent.scss";
-import LoadIndicatedContentSpinner from "./LoadIndicatedContentSpinner";
 
-const loadingClassName = "loading-process";
-const endingClassName = "ending-process";
-const contentReadyClassName = "content-is-ready";
+const enum State {
+  Initial = "initial-state",
+  Loading = "loading-process",
+  Loaded = "content-loaded",
+  Ending = "ending-process",
+  Ready = "content-is-ready",
+}
 
 export default function LoadIndicatedContent(
   props: PropsWithChildren<{ isLoading: boolean; onLoad?: () => void }>
 ) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isReady, setIsReady] = useState(false);
-  const [isEnding, setIsEnding] = useState(false);
+  const [state, setState] = useState(State.Initial);
   const initTime = useMemo(() => Date.now(), []);
-  const container = useRef<HTMLDivElement>(null);
 
   const onEnd = () => {
-    if (container.current) {
-      container.current.classList.remove(endingClassName, loadingClassName);
-      container.current.classList.add(contentReadyClassName);
-    }
-    setTimeout(() => {
-      setIsEnding(false);
-      setIsLoading(false);
-      setIsReady(true);
-      props.onLoad && props.onLoad();
-    });
+    setState(State.Loaded);
+    props.onLoad && props.onLoad();
   };
 
   useEffect(() => {
-    if (props.isLoading) setIsLoading(true);
+    if (props.isLoading) setState(State.Loading);
     else {
-      if (Date.now() - initTime < 30) onEnd();
-      else {
-        setIsReady(true);
-        setTimeout(() => setIsLoading(false), 1100);
-      }
+      if (Date.now() - initTime < 100) onEnd();
+      else setState(State.Ready);
     }
   }, [props.isLoading]);
 
   return (
-    <div
-      className={`load-indicated-content-container ${
-        isLoading && !isEnding
-          ? loadingClassName
-          : isEnding
-          ? endingClassName
-          : isReady
-          ? contentReadyClassName
-          : ""
-      }`}
-      ref={container}
-    >
+    <div className={`load-indicated-content-container ${state}`}>
       <div
         className="load-indicated-content-spinner-container"
-        onAnimationEnd={() => isEnding && onEnd()}
+        onAnimationEnd={() => state === State.Ending && onEnd()}
       >
-        <LoadIndicatedContentSpinner
-          onAnimationIteration={() => isReady && setIsEnding(true)}
-        />
+        <div
+          onAnimationIteration={() =>
+            state === State.Ready && setState(State.Ending)
+          }
+          className="load-indicated-content-spinner"
+        >
+          <svg
+            x="0px"
+            y="0px"
+            viewBox="0 0 306 386.64"
+            className="smile-container"
+          >
+            <path
+              className="smile-dot"
+              d="M82.54,2.24c13.69,5.94,19.98,21.85,14.05,35.55S74.74,57.77,61.04,51.83C47.35,45.9,41.06,29.98,47,16.29
+        C52.93,2.59,68.84-3.7,82.54,2.24z"
+            />
+            <path
+              className="smile-line"
+              d="M160.32,30.33c68.8,21.21,118.35,85.23,118.35,160.92c0,93-75.47,168.39-168.57,168.39
+	c-30.08,0-58.32-7.87-82.76-21.66"
+            />
+          </svg>
+        </div>
       </div>
-      {isLoading ? null : props.children}
+      {state === State.Loaded ? props.children : null}
     </div>
   );
 }
