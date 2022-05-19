71c9d204b813e232137522a5d905276baf6244a1
PSlobodskoy <PSlobodskoy@one.kz>
2022-05-19 11:37:38 +0300

    update admin users per 60s

--- src/components/apps/admin/Admin.tsx
+++ src/components/apps/admin/Admin.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import "./Admin.scss";
 import UserFace from "./complect/users/UserFace";
 import useUsers from "./complect/users/useUsers";
 import PhaseAdminEditorContainer from "./phase-editor-container/PhaseAdminEditorContainer";
 
+let timeOut: any;
+let updates = 0;
+
 export default function Admin() {
   const { users } = useUsers();
   const usersUses = useUsers();
+  const [, setUpdates] = useState(0);
+
+  useEffect(() => {
+    const set = () => {
+      setUpdates(++updates);
+      clearTimeout(timeOut);
+      timeOut = setTimeout(set, 60000);
+    };
+    set();
+    return () => clearTimeout(timeOut);
+  }, []);
 
   return (
     <PhaseAdminEditorContainer
