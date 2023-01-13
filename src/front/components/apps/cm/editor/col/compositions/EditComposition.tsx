b962bf0aa519b80aa8f3fc16eb9adfc3c6ac6ed4
PSlobodskoy <PSlobodskoy@one.kz>
2023-01-13 13:46:29 +0300

    cm.com.editor. non swipeable area

--- src/front/components/apps/cm/editor/col/compositions/EditComposition.tsx
+++ src/front/components/apps/cm/editor/col/compositions/EditComposition.tsx
@@ -1,7 +1,6 @@
 import EvaIcon from "../../../../../../complect/eva-icon/EvaIcon";
 import useExer from "../../../../../../complect/exer/useExer";
 import { NavigationThrowNodeProps } from "../../../../../../complect/nav-configurer/Navigation.model";
-import SwipeableContainer from "../../../../../../complect/swipeable/SwipeableContainer";
 import useCmNav from "../../../base/useCmNav";
 import { cmExer } from "../../../Cm.store";
 import { editCompositionNavs } from "../../editorNav";
@@ -34,57 +33,41 @@ export default function EditComposition({
             </div>
           </div>
         ) : (
-          <SwipeableContainer
-            onHorizontalSwipe={(dir) => {
-              const phasei = editCompositionNavs.findIndex(
-                ({ phase: [phase] }) => phase === currentChildPhase
-              );
-
-              if ("l" === dir && phasei < editCompositionNavs.length - 1)
-                goTo(editCompositionNavs[phasei + 1].phase, relativePoint);
-
-              if ("r" === dir && phasei > 0)
-                goTo(editCompositionNavs[phasei - 1].phase, relativePoint);
-            }}
-            content={
-              <>
-                <div className="flex around margin-gap">
-                  {editCompositionNavs.map(
-                    ({
-                      data: { icon, iconText } = {},
-                      phase: [phase],
-                      accessRule,
-                    }) => {
-                      if (accessRule && !cmExer.actionAccessedOrNull(accessRule))
-                        return null;
-                      return (
-                        <span
-                          key={`editCompositionNavs ${phase}`}
-                          className="pointer"
-                          onClick={() =>
-                            goTo(phase, relativePoint, ccom.isCreated)
+          <>
+            <div className="flex around margin-gap">
+              {editCompositionNavs.map(
+                ({
+                  data: { icon, iconText } = {},
+                  phase: [phase],
+                  accessRule,
+                }) => {
+                  if (accessRule && !cmExer.actionAccessedOrNull(accessRule))
+                    return null;
+                  return (
+                    <span
+                      key={`editCompositionNavs ${phase}`}
+                      className="pointer"
+                      onClick={() =>
+                        goTo(phase, relativePoint, ccom.isCreated)
+                      }
+                    >
+                      {icon ? (
+                        <EvaIcon
+                          name={
+                            `${icon}${phase === currentChildPhase ? "" : "-outline"
+                            }` as never
                           }
-                        >
-                          {icon ? (
-                            <EvaIcon
-                              name={
-                                `${icon}${
-                                  phase === currentChildPhase ? "" : "-outline"
-                                }` as never
-                              }
-                            />
-                          ) : (
-                            iconText
-                          )}
-                        </span>
-                      );
-                    }
-                  )}
-                </div>
-                {outletContent}
-              </>
-            }
-          />
+                        />
+                      ) : (
+                        iconText
+                      )}
+                    </span>
+                  );
+                }
+              )}
+            </div>
+            {outletContent}
+          </>
         )
       }
     />
