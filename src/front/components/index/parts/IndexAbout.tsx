2989e9c913a5e19b2c7f771bee840afba645fdf2
PSlobodskoy <PSlobodskoy@one.kz>
2023-01-22 20:23:27 +0300

    IndexAbout. qr-links added

--- src/front/components/index/parts/IndexAbout.tsx
+++ src/front/components/index/parts/IndexAbout.tsx
@@ -1,38 +1,10 @@
-import { useEffect, useMemo, useState } from "react";
-import { useSelector } from "react-redux";
 import EvaIcon from "../../../complect/eva-icon/EvaIcon";
-import { indexStorage } from "../../../shared/jstorages";
-import { RootState } from "../../../shared/store";
+import QRCode from "../../../complect/qr-code/QRCode";
 import * as versionNum from '../../../version.json';
 
 const version = { ...versionNum };
 
-const appsSelector = (state: RootState) => state.index.apps;
-const currentAppSelector = (state: RootState) => state.index.currentApp;
-
 export default function IndexAbout() {
-  const apps = useSelector(appsSelector);
-  const currentAppName = useSelector(currentAppSelector);
-  const { index, [currentAppName]: app } = indexStorage.get("lastUpdates") || {};
-  const [indexLastUpdate, setIdexLastUpdate] = useState(index);
-  const [appLastUpdate, setAppLastUpdate] = useState(app);
-
-  indexStorage.listen("lastUpdates", "IndexAbout", (val) => {
-    const { index, [currentAppName]: app } = val || {};
-    setIdexLastUpdate(index);
-    setAppLastUpdate(app);
-  });
-
-  useEffect(() => () => { indexStorage.mute("lastUpdates", "IndexAbout"); }, []);
-
-  const latsDate = useMemo(() => {
-    const lastTimestamp = Math.max(indexLastUpdate || 0, appLastUpdate || 0);
-    return lastTimestamp > 0
-      ? new Date(lastTimestamp).toLocaleString()
-      : null;
-  }, [currentAppName, indexLastUpdate, appLastUpdate]);
-
-  const currentApp = apps.find((app) => app.name === currentAppName);
 
   return (
     <div className="full-container flex center">
@@ -40,19 +12,23 @@ export default function IndexAbout() {
         className="flex custom-align-items column"
         onClick={(event) => event.stopPropagation()}
       >
-        <div className="flex margin-gap-v">
-          <EvaIcon name="telegram" />
-          <a href="https://t.me/jesmyl_space">@jesmyl_space</a>
+        <div className="padding-giant-gap">
+          <QRCode text="https://t.me/jesmyl_space" />
+          <div className="flex center">
+            <EvaIcon name="telegram" />
+            <a href="https://t.me/jesmyl_space">@jesmyl_space</a>
+          </div>
         </div>
-        <div className="flex">
-          <EvaIcon name="telegram" />
-          <a href="https://t.me/danikpon">дизайн (3</a>
+        <div className="padding-giant-gap">
+          <QRCode text="https://t.me/danikpon" />
+          <div className="flex center">
+            <EvaIcon name="telegram" />
+            <a href="https://t.me/danikpon">дизайн (3</a>
+          </div>
         </div>
       </div>
-      <div className="absolute pos-bottom margin-big-gap-v flex center full-width">
-        <span>
-          {currentApp && latsDate && `${currentApp.title} ${latsDate}; v${version.num}`}
-        </span>
+      <div className="absolute pos-bottom padding-giant-gap">
+        v{version.num}
       </div>
     </div>
   );
