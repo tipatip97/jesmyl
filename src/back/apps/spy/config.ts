60df2fec2d14a6058097b7537271d83c74860d06
PSlobodskoy <PSlobodskoy@one.kz>
2023-01-22 18:47:04 +0300

    config files is updated

--- src/back/apps/spy/config.ts
+++ src/back/apps/spy/config.ts
@@ -3,7 +3,7 @@ import { FilerAppConfig } from "../../complect/filer/Filer.model";
 const config: FilerAppConfig = {
     title: 'Аккорд Мастер',
     requirements: [
-        "cache"
+        "locations", "rooms"
     ]
 }
 
