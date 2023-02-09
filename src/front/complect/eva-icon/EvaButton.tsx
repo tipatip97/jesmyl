9c0d4c2518aa9d4ff7a9ecf5165420bbdea79c1a
PSlobodskoy <PSlobodskoy@one.kz>
2023-02-09 17:11:27 +0300

    EvaButton. can confirm action

--- src/front/complect/eva-icon/EvaButton.tsx
+++ src/front/complect/eva-icon/EvaButton.tsx
@@ -1,24 +1,27 @@
 import { HTMLAttributes } from "react";
 import * as pack from "../../resource/eva-icons.json";
+import modalService from "../modal/Modal.service";
 import EvaIcon from "./EvaIcon";
 
 export type EvaIconName = keyof typeof pack;
 
 export default function EvaButton(
   props: HTMLAttributes<HTMLOrSVGElement> & {
-    name: EvaIconName;
-    alt?: string;
-    disabled?: boolean;
+    name: EvaIconName,
+    alt?: string,
+    disabled?: boolean,
+    confirm?: string,
   }
 ) {
   return (
     <>
       <EvaIcon
         {...props}
-        className={`pointer ${props.disabled ? "disabled" : ""} ${
-          props.className
-        }`}
-        onClick={props.disabled ? undefined : props.onClick}
+        className={`pointer ${props.disabled ? "disabled" : ""} ${props.className}`}
+        onClick={props.disabled ? undefined : props.onClick && (async (event) => {
+          if (!props.confirm || await modalService.confirm(props.confirm))
+            props.onClick!(event);
+        })}
       />
     </>
   );
