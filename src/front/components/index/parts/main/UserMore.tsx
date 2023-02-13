272d61a0704c7bd9193832f0f9c429fc47370d18
PSlobodskoy <PSlobodskoy@one.kz>
2023-02-13 10:42:59 +0300

    Index. remove "mail to developers" button

--- src/front/components/index/parts/main/UserMore.tsx
+++ src/front/components/index/parts/main/UserMore.tsx
@@ -1,28 +1,12 @@
 import EvaIcon from "../../../../complect/eva-icon/EvaIcon";
-import useFullscreenContent from "../../../../complect/fullscreen-content/useFullscreenContent";
 import modalService from "../../../../complect/modal/Modal.service";
 import useAuth from "../../useAuth";
-import MailToDevelopers from "./MailToDevelopers";
 
 export default function UserMore() {
-  const { openFullscreenContent, closeFullscreenContent } =
-    useFullscreenContent();
   const { logout } = useAuth();
 
   return (
     <>
-      <div
-        className="abs-item pointer"
-        onClick={() => {
-          openFullscreenContent(
-            <MailToDevelopers close={closeFullscreenContent} />
-          );
-        }}
-      >
-        <EvaIcon name="email-outline" className="abs-icon" />
-        <div>Написать разработчикам</div>
-        <div className="abs-action" />
-      </div>
       <div
         className="abs-item pointer"
         onClick={(event) => {
