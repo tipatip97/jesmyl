973541cf0b4990fdd42607f1fb3b455ef111d641
PSlobodskoy <PSlobodskoy@one.kz>
2023-02-14 21:56:02 +0300

    cm. correct chord regexp

--- src/front/components/apps/cm/editor/Editor.complect.tsx
+++ src/front/components/apps/cm/editor/Editor.complect.tsx
@@ -1,6 +1,6 @@
 export const chordLikeStr =
   "([ACDFG]#?|[EH])(\\+|11|((m|min|7?sus|maj|dim|add)?(\\d(/\\d)?)?))";
-export const textedChordReg = new RegExp(`(^-?${chordLikeStr}(/${chordLikeStr})?$)|(^\\.+-?${chordLikeStr}(/${chordLikeStr})?((\\.+|-|\\.+-)${chordLikeStr}(/${chordLikeStr})?)*$)`);
+export const textedChordReg = new RegExp(`^\\.*-?${chordLikeStr}(/${chordLikeStr})?((\\.+|-|\\.+-)${chordLikeStr}(/${chordLikeStr})?)*$`);
 export const correctChordNameReg = new RegExp(`^${chordLikeStr}(/${chordLikeStr})?$`);
 export const checkIsChordLineReg = /^[-+A-Ha-z# /\d]+$/;
 export const slavicLowerLettersStr = 'а-яёіґїє';
