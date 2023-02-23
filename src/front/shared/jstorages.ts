439d956d3d7142126e31ebf9678b5dd32a03b924
PSlobodskoy <PSlobodskoy@one.kz>
2023-02-23 17:25:37 +0300

    created storage stum

--- src/front/shared/jstorages.ts
+++ src/front/shared/jstorages.ts
@@ -1,17 +1,21 @@
 import { JStorageName } from "../app/App.model";
 import { JStorage } from "../complect/JStorage";
+import { NavigationStorage } from "../complect/nav-configurer/Navigation.model";
 import admin from "../components/apps/admin/adminStorage";
 import cm from "../components/apps/cm/cmStorage";
 import leader from "../components/apps/leader/leaderStorage";
 import spy from "../components/apps/spy/spyStorage";
 import index from "../components/index/indexStorage";
 
+interface Sto { }
+const stub: JStorage<NavigationStorage<Sto>> = new JStorage<NavigationStorage<Sto>>('complect');
+
 export const appStorage: Record<JStorageName, JStorage<any>> = {
     index,
-    tuner: index,
+    tuner: stub,
     admin,
     spy,
-    complect: index,
+    complect: stub,
     cm,
     leader,
 };
