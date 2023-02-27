d72c7ee38d929b5b48622bb7bf9948134fcd5cf6
PSlobodskoy <PSlobodskoy@one.kz>
2023-02-27 15:03:00 +0300

    admin. config

--- src/back/apps/admin/config.ts
+++ src/back/apps/admin/config.ts
@@ -8,6 +8,7 @@ const config: FilerAppConfig = {
             name: "userList",
             rootPath: 'bonjour',
             ext: null,
+            refreshTrigger: 'register',
         },
     ]
 }
