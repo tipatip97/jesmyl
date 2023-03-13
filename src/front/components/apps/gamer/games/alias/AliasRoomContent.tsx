31e9372c2098eb7c05e3786f83d790917dc492cd
PSlobodskoy <PSlobodskoy@one.kz>
2023-03-13 10:51:07 +0300

    gamer.alias. initial manager conent usage

--- src/front/components/apps/gamer/games/alias/AliasRoomContent.tsx
+++ src/front/components/apps/gamer/games/alias/AliasRoomContent.tsx
@@ -1,8 +1,13 @@
 import GamerRoomMemberList from "../../complect/GamerRoomMemberList";
+import useGamerRooms from "../../complect/rooms/room/useGamerRooms";
+import AliasRoomInitialManagerContent from "./AliasRoomInitialManagerContent";
 
 export default function AliasRoomContent() {
+    const { memberPossibilities } = useGamerRooms();
+    const myPossibilities = memberPossibilities();
 
     return <>
         <GamerRoomMemberList />
+        {myPossibilities.isManager && <AliasRoomInitialManagerContent />}
     </>;
 }
