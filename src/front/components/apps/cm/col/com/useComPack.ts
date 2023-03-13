d44343d29decc1621236beec9c4c914417e3c74f
PSlobodskoy <PSlobodskoy@one.kz>
2023-03-13 10:39:52 +0300

    cm. com pack fix

--- src/front/components/apps/cm/col/com/useComPack.ts
+++ src/front/components/apps/cm/col/com/useComPack.ts
@@ -1,30 +1,24 @@
-import { useMemo } from "react";
 import useCmNav from "../../base/useCmNav";
 import useSelectedComs from "../../base/useSelectedComs";
 import { useMarks } from "../../lists/marks/useMarks";
 import { useMeetings } from "../../lists/meetings/useMeetings";
-import { useCcat } from "../cat/useCcat";
 import { Com } from "./Com";
-
+import { useCcom } from "./useCcom";
 
 export default function useComPack(): [Com[] | nil, string] {
     const { markedComs } = useMarks();
     const { currentMeeting } = useMeetings();
     const { route } = useCmNav();
-    const { takeSelectedComs, selectedComws } = useSelectedComs();
-    const cat = useCcat(route?.includes('all'));
+    const ccom = useCcom();
+    const { takeSelectedComs } = useSelectedComs();
 
-    return useMemo(() => {
-        return route?.includes('marks')
-            ? [markedComs, " - Избранное"]
-            : currentMeeting && route?.includes('meetings')
-                ? [currentMeeting.coms, " - " + currentMeeting.name]
-                : route?.includes('selected')
-                    ? [takeSelectedComs(), " - Выбранное"]
-                    : route?.includes('all')
-                        ? cat
-                            ? [cat.wraps.map(wrap => wrap.com), " - " + cat.name]
-                            : [null, '']
-                        : [null, ""];
-    }, [cat, currentMeeting, markedComs, route, selectedComws]);
+    return route?.includes('marks')
+        ? [markedComs, " - Избранное"]
+        : currentMeeting && route?.includes('meetings')
+            ? [currentMeeting.coms, " - " + currentMeeting.name]
+            : route?.includes('selected')
+                ? [takeSelectedComs(), " - Выбранное"]
+                : ccom
+                    ? [[ccom], '']
+                    : [null, ''];
 }
