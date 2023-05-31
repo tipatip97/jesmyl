4aadc25c42841708fb4161badfdbf8a93644bd33
PSlobodskoy <PSlobodskoy@one.kz>
2023-05-31 08:12:41 +0300

    useIsRedactArea. replaced

---
+++ src/front/complect/useIsRedactArea.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import EvaButton from "./eva-icon/EvaButton";
+
+export default function useIsRedactArea(
+  redactable: boolean | nil,
+  redact: boolean | nil,
+  canRedact?: boolean | nil,
+  isShowDoneButton?: boolean | nil,
+  onEditStart?: ((isSelfRedact: boolean) => void) | nil,
+) {
+  const [isSelfRedact, setIsSelfRedact] = useState(false);
+  const isCanRedact = !!(canRedact == null || canRedact);
+
+  const ret = {
+    isSelfRedact,
+    isRedact:
+      isCanRedact &&
+      (redact ?? (redactable == null ? isSelfRedact : redactable && isSelfRedact)),
+    setIsSelfRedact,
+    editIcon: redactable
+      ? isCanRedact && !(redact ?? isSelfRedact)
+        ? <EvaButton
+          name="edit-outline"
+          onClick={() => {
+            if (redact == null) setIsSelfRedact(true);
+            onEditStart?.(true);
+          }}
+        />
+        : isShowDoneButton && <EvaButton
+          name="checkmark-circle-2-outline"
+          className="color--ok"
+          disabled={redact === true && !isSelfRedact}
+          onClick={() => {
+            setIsSelfRedact(false);
+            onEditStart?.(false);
+          }}
+        />
+      : null,
+  };
+
+  return ret;
+}
