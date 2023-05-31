4cb752c62ed3f66f93c1bcc86a81615dd7efbf7d
PSlobodskoy <PSlobodskoy@one.kz>
2023-05-31 07:59:48 +0300

    back. models addition

--- src/back/models.ts
+++ src/back/models.ts
@@ -1,18 +1,33 @@
 import { ExecutionMethod, ExecutionSidesDict, ExecutionTrack } from "./complect/executer/Executer.model";
+import { actionBoxSetSystems } from "./values";
 
 export interface ActionBox extends Partial<Record<`/${string}`, ActionBox>>, Partial<Record<`<${string}>`, ActionBox>> {
     level?: number,
-    action?: string,
+    scopeNode?: string,
     shortTitle?: string,
     title?: string,
     access?: string,
-    method?: ExecutionMethod,
     uniqs?: string[],
-    value?: any,
-    args?: Record<string, string | any[]>
     fixAccesses?: Record<string, ExecutionTrack>,
     side?: ExecutionSidesDict,
     expected?: [] | {},
     setInEachValueItem?: Record<string, Record<string, unknown>>,
     isSequre?: boolean,
+    C?: ActionCRUD,
+    U?: ActionCRUD,
+    D?: ActionCRUD,
+
+    action?: string,
+    method?: ExecutionMethod,
+    value?: any,
+    args?: Record<string, string | any[]>,
 };
+
+export interface ActionCRUD {
+    method?: ExecutionMethod,
+    value?: any,
+    args?: Record<string, string | any[]>,
+    setSystems?: ActionBoxSetSystems[],
+}
+
+export type ActionBoxSetSystems = keyof typeof actionBoxSetSystems;
