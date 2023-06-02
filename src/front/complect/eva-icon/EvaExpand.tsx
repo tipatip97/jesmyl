a24f45b67774caeed5a6cb732eb54e9a2c496b50
PSlobodskoy <PSlobodskoy@one.kz>
2023-06-02 13:04:28 +0300

    EvaExpand. init

---
+++ src/front/complect/eva-icon/EvaExpand.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import EvaIcon from "./EvaIcon";
+
+export default function EvaExpand({
+    isExpand,
+    set,
+    prefix,
+}: {
+    prefix?: ReactNode,
+    isExpand: boolean,
+    set: (val: boolean) => void
+}) {
+    return prefix != null
+        ? <div
+            className="flex flex-gap pointer"
+            onClick={() => set(!isExpand)}
+        >
+            {prefix}
+            <EvaIcon name={isExpand ? 'chevron-up' : 'chevron-down'} />
+        </div>
+        : <EvaIcon
+            name={isExpand ? 'chevron-up' : 'chevron-down'}
+            onClick={() => set(!isExpand)}
+        />;
+}
