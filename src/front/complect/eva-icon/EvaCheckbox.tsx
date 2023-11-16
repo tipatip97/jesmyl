47b65af8c33456608c77213ac44744ff8c4de3ca
PSlobodskoy <PSlobodskoy@one.kz>
2023-11-16 09:14:08 +0300

    Eva. EvaCheckbox added

---
+++ src/front/complect/eva-icon/EvaCheckbox.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+import EvaIcon from "./EvaIcon";
+
+export default function EvaCheckbox(
+  props: {
+    checked?: boolean,
+    onChange?: (is: boolean) => void,
+    disabled?: boolean,
+    prefix?: null | ReactNode,
+    postfix?: null | ReactNode,
+    className?: string,
+  }
+) {
+  const isClickable = !props.disabled ? true : undefined;
+  const className = `${props.className || ''}${isClickable ? ' pointer' : ''}${props.disabled ? ' disabled' : ''}`;
+  const name = props.checked ? 'checkmark-square-2-outline' : 'square-outline';
+
+  return props.prefix === undefined && props.postfix === undefined
+    ? <EvaIcon
+      name={name}
+      className={className}
+      onClick={isClickable && props.onChange && (() => props.onChange!(!props.checked))}
+    />
+    : <span
+      className={`flex flex-gap ${className || 'flex-max'}`}
+      onClick={isClickable && props.onChange && (() => props.onChange!(!props.checked))}
+    >
+      {props.prefix}
+      <EvaIcon name={name} />
+      {props.postfix}
+    </span>;
+}
