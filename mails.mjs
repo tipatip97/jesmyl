d6f5fbdebbb5ea1c21e4560f1a210359ee561fff
PSlobodskoy <PSlobodskoy@one.kz>
2023-11-21 15:29:35 +0300

    tg-bot+nodemailer: init

---
+++ mails.mjs
@@ -0,0 +1,27 @@
+
+import nodemailer from 'nodemailer';
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.mail.ru',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'jesmyl.job@mail.ru',
+        pass: 'kDG4prazwJcvJ4p74EbW',
+    },
+});
+
+var mailOptions = {
+    from: 'Ski <jesmyl.job@mail.ru>',
+    to: '79787694218@mail.ru',
+    subject: 'Sending Email using Node.js',
+    html: `<h2>Приветик всеммм!!!</h2><strong>${new Date().toISOString()}</strong>`,
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
