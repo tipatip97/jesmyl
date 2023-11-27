5d823c25139cfc83732f562f62d6e1eb1ef1c977
PSlobodskoy <PSlobodskoy@one.kz>
2023-11-27 20:09:00 +0300

    randoms: adds

--- src/front/complect/random-twice-name/getRandomTwiceName.ts
+++ src/front/complect/random-twice-name/getRandomTwiceName.ts
@@ -1,36 +1,22 @@
 import mylib from "../my-lib/MyLib";
-import * as wordStorage from './randomTwiceNameStorage.json';
+import { randomTwiceNameStorage } from './randomTwiceNameStorage';
+import { replacePronounEnd } from "./wordRegEnds";
 
-const storage = wordStorage as Record<'nouns' | 'pronouns', string[]>;
+const storage = {
+    nouns: Object.keys(randomTwiceNameStorage.nouns).slice(1),
+    pronouns: Object.keys(randomTwiceNameStorage.pronouns).slice(1),
+};
 
-export const getRandomTwiceName = (topPronoun?: string | null, topNoun?: string) => {
-    let pronoun = topPronoun ??
-        storage.pronouns[mylib.randomOf(0, storage.pronouns.length - 1)];
+export const getRandomTwiceName = (topPronoun?: string | nil, topNoun?: string | nil): string[] => {
+    const pronoun = topPronoun ?? storage.pronouns[mylib.randomOf(0, storage.pronouns.length - 1)];
     const noun = topNoun ?? storage.nouns[mylib.randomOf(0, storage.nouns.length - 1)];
 
-    if (/[ыи,]$/.exec(noun)) { // весенний быстрый скоростной учащийся
-        pronoun = pronoun
-            .replace(/йся$/, 'еся')
-            .replace(/ичий/, 'ичьи')
-            .replace(/ый$/, 'ые')
-            .replace(/ий$/, 'ие')
-            .replace(/ой$/, 'ые');
-    } else if (/[аяь:]$/.exec(noun)) {
-        pronoun = pronoun
-            .replace(/ичий/, 'ичья')
-            .replace(/([жшчщ])[иоы]й/, '$1ая')
-            .replace(/ний$/, 'няя')
-            .replace(/[иоы]й$/, 'ая');
-    } else if (/[оеё.]$/.exec(noun)) {
-        pronoun = pronoun
-            .replace(/ний$/, 'нее')
-            .replace(/ичий/, 'ичье')
-            .replace(/([жшчщ])[иоы]й/, '$1ее')
-            .replace(/[иоы]й$/, 'ое');
-    }
-
-    return [pronoun, noun.replace(/[^- а-яё\d"]/ig, '') + (noun.startsWith('"') ? '"' : '')];
+    return replacePronounEnd(pronoun, noun);
 }
+
+
+
+// for (let i = 0; i < 10; i++) console.info(getRandomTwiceName().join(' '));
 
 // console.info(getRandomTwiceName('птичий', 'устройство').join(' '));
 
---
+++ src/front/complect/random-twice-name/wordRegEnds.ts
@@ -0,0 +1,123 @@
+import mylib, { MyLib } from "../my-lib/MyLib";
+
+type Replacer<Ret> = (substring: string, ...args: any[]) => Ret;
+
+interface EndVariantsDict {
+    pronoun: string | Replacer<Replacer<string>>,
+    verb_pronoun: string | Replacer<Replacer<string>>,
+    verb_noun: string | Replacer<Replacer<string>>,
+}
+
+const wordRegEnds: Record<string, Record<string, EndVariantsDict>> = {
+    '([ыи])$|,$': {
+        'йся$': { // отвернувшийся
+            pronoun: 'еся', // отвернувшиеся
+            verb_pronoun: 'хся',
+            verb_noun: '',
+        },
+        'ичий': { // птичий
+            pronoun: 'ичьи', // птичьи
+            verb_pronoun: 'ичьих',
+            verb_noun: '',
+        },
+        'ый$': { // гордый
+            pronoun: 'ые', // гордые
+            verb_pronoun: 'ых',
+            verb_noun: '',
+        },
+        'ий$': { // синий
+            pronoun: 'ие', // синие
+            verb_pronoun: 'их',
+            verb_noun: '',
+        },
+        'ой$': { // простой
+            pronoun: 'ые', // простые
+            verb_pronoun: 'ых',
+            verb_noun: '',
+        },
+    },
+    '([аяь])$|(.):$': {
+        'ичий': { // охотничий
+            pronoun: 'ичья', // охотничья
+            verb_pronoun: 'ичьей',
+            verb_noun: '',
+        },
+        '([жшчщ])[иоы]й': {  // большой
+            pronoun: '$1ая', // большая
+            verb_pronoun: '$1ой',
+            verb_noun: '',
+        },
+        'ний$': { // синий
+            pronoun: 'няя', // синяя
+            verb_pronoun: 'ней',
+            verb_noun: '',
+        },
+        '([иоы])й$': { // скорый
+            pronoun: 'ая', // скорая
+            verb_pronoun: 'й',
+            verb_noun: 'ы',
+        },
+    },
+    '([оеё])$|\\.$': {
+        'ний$': { // ранний
+            pronoun: 'нее', // раннее
+            verb_pronoun: 'него',
+            verb_noun: '',
+        },
+        'ичий': { // птичий
+            pronoun: 'ичье', // птичье
+            verb_pronoun: 'ичьего',
+            verb_noun: '',
+        },
+        '([жчщ])[иоы]й': { // большой поющий
+            pronoun: '$1ее', // большее поющее
+            verb_pronoun: '$1его',
+            verb_noun: '',
+        },
+        '[иоы]й$': { // холостой
+            pronoun: 'ое', // холостое
+            verb_pronoun: 'ого',
+            verb_noun: '',
+        },
+    },
+};
+
+const regEnds: [RegExp, [RegExp, EndVariantsDict][]][] =
+    MyLib.entries(wordRegEnds).map(([end, variants]) => [
+        new RegExp(end),
+        MyLib.entries(variants).map(([end, dict]) => [new RegExp(end), dict]),
+    ]);
+
+console.log(regEnds);
+
+const allAll = (all: string) => all;
+
+const fixNoun = (() => {
+    const reg = /[^- а-яё\d"]/ig;
+    return (noun: string) => noun.replace(reg, '') + (noun.startsWith('"') ? '"' : '');
+})();
+
+export const replacePronounEnd = (pronoun: string, noun: string): [string, string] => {
+    for (let i = 0; i < regEnds.length; i++) {
+        const match = regEnds[i][0].exec(noun);
+        if (match) {
+            const regEnd = regEnds[i][1];
+
+            for (let j = 0; j < regEnd.length; j++) {
+                if (regEnd[j][0].exec(pronoun)) {
+                    const invoke = (funcOrString: any) => {
+                        return mylib.isFunc(funcOrString)
+                            ? funcOrString(...match)
+                            : funcOrString || allAll;
+                    };
+
+                    return [
+                        pronoun.replace(regEnd[j][0], invoke(regEnd[j][1].pronoun)),
+                        fixNoun(noun),
+                    ];
+                }
+            }
+        }
+    }
+    return [pronoun, fixNoun(noun)];
+};
