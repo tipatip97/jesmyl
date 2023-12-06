b897a61a226921a07eac488e23fa6ac3a26ea897
PSlobodskoy <PSlobodskoy@one.kz>
2023-12-06 14:22:56 +0300

    scheddule widget: alarm form admin visibility

--- src/front/complect/schedule-widget/alarm/Alarm.tsx
+++ src/front/complect/schedule-widget/alarm/Alarm.tsx
@@ -3,12 +3,12 @@ import { useSchedules } from "../useScheduleWidget";
 import "./Alarm.scss";
 import ScheduleWidgetAlarmContent from "./AlarmContent";
 
-export default function ScheduleWidgetAlarm({ onGoTo }: { onGoTo: () => void }) {
+export default function ScheduleWidgetAlarm({ onGoTo, isForceShow }: { onGoTo: () => void, isForceShow?: boolean }) {
     const schedules = useSchedules();
     const nav = useIndexNav();
 
     return <>
-        {schedules.list.some((schedule) => schedule.start)
+        {(isForceShow || schedules.list.some((schedule) => schedule.start))
             && <ScheduleWidgetAlarmContent
                 onGoTo={onGoTo}
                 observeSchw={nav.appRouteData.schw}
