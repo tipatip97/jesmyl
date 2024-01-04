23cdf81c8af1bf7e33303d09234639b04572d45e
PSlobodskoy <PSlobodskoy@one.kz>
2024-01-04 09:03:09 +0300

    fix(Portal): some optimizations

--- src/front/complect/popups/[complect]/Portal.tsx
+++ src/front/complect/popups/[complect]/Portal.tsx
@@ -1,17 +1,20 @@
 import { ReactNode, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const containerInitState = () => document.createElement('div');
+
 export default function Portal({ children }: { children: ReactNode }) {
-  const [container] = useState(() => document.createElement('div'));
+  const [container] = useState(containerInitState);
 
   useEffect(() => {
     const parentContainer = document.querySelector('.application-container');
-    parentContainer?.appendChild(container);
+    if (parentContainer === null) return;
+    parentContainer.appendChild(container);
 
     return () => {
-      parentContainer?.removeChild(container);
+      parentContainer.removeChild(container);
     };
-  }, []);
+  }, [container]);
 
   return ReactDOM.createPortal(children, container);
 }
