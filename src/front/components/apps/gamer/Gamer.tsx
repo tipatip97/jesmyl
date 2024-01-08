e909c00d878c9a246a37ea86a7333ab6f646d215
PSlobodskoy <PSlobodskoy@one.kz>
2024-01-08 08:13:10 +0300

    fix(gamer): new gamer room screen actual open-hook

--- src/front/components/apps/gamer/Gamer.tsx
+++ src/front/components/apps/gamer/Gamer.tsx
@@ -1,11 +1,11 @@
 import { useBottomPopup } from '../../../complect/absolute-popup/bottom-popup/useBottomPopup';
-import GamerMore from './complect/GamerMore';
+import { GamerMore } from './complect/GamerMore';
 import PhaseGamerContainer from './complect/PhaseGamerContainer';
 import GamerOfflineRoomList from './complect/rooms/offline-room/GamerOfflineRoomList';
 import GamerRoomList from './complect/rooms/room/GamerRoomList';
 
 export default function Gamer() {
-  const [popupNode, openPopup] = useBottomPopup(() => <GamerMore />);
+  const [popupNode, openPopup] = useBottomPopup(GamerMore);
 
   return (
     <PhaseGamerContainer
--- src/front/components/apps/gamer/complect/GamerMore.tsx
+++ src/front/components/apps/gamer/complect/GamerMore.tsx
@@ -1,38 +1,47 @@
 import { bottomPopupContentPreparer } from '../../../../complect/absolute-popup/bottom-popup/item-preparer';
-import useFullscreenContent from '../../../../complect/fullscreen-content/useFullscreenContent';
+import { BottomPopupContenter } from '../../../../complect/absolute-popup/bottom-popup/model';
+import useFullContent from '../../../../complect/fullscreen-content/useFullContent';
 import modalService from '../../../../complect/modal/Modal.service';
 import useAuth from '../../../index/useAuth';
 import { gamerExer } from '../Gamer.store';
 import NewGamerRoomScreen from './rooms/NewGamerRoomScreen';
 import { useGamerOfflineRoomsPassport } from './rooms/offline-room/hooks/passport';
 
-export default function GamerMore() {
+export const GamerMore: BottomPopupContenter = () => {
   const auth = useAuth();
-  const { openFullscreenContent } = useFullscreenContent();
+  const [newRoomMakerNode, openNewRoomMaker] = useFullContent(close => <NewGamerRoomScreen close={close} />);
+  const [newRoomOffMakerNode, openNewOffRoomMaker] = useFullContent(close => (
+    <NewGamerRoomScreen
+      close={close}
+      offline
+    />
+  ));
   const passport = useGamerOfflineRoomsPassport();
 
-  return bottomPopupContentPreparer({
-    items: [
-      gamerExer.actionAccessedOrNull('addNewRoom', auth) && {
-        title: 'Новая комната',
-        icon: 'plus',
-        onClick: () => openFullscreenContent(close => <NewGamerRoomScreen close={close} />),
-      },
-      {
-        title: 'Новая оффлайн комната',
-        icon: 'plus',
-        className: passport ? '' : ' fade-05',
-        onClick: () => {
-          if (passport)
-            openFullscreenContent(close => (
-              <NewGamerRoomScreen
-                close={close}
-                offline
-              />
-            ));
-          else modalService.alert('Нужно создать паспорт для добавления оффлайн комнат', 'Пойми!');
-        },
-      },
-    ],
-  });
-}
+  return [
+    <>
+      {newRoomMakerNode}
+      {newRoomOffMakerNode}
+    </>,
+    <>
+      {bottomPopupContentPreparer({
+        items: [
+          gamerExer.actionAccessedOrNull('addNewRoom', auth) && {
+            title: 'Новая комната',
+            icon: 'plus',
+            onClick: () => openNewRoomMaker(),
+          },
+          {
+            title: 'Новая оффлайн комната',
+            icon: 'plus',
+            className: passport ? '' : ' fade-05',
+            onClick: () => {
+              if (passport) openNewOffRoomMaker();
+              else modalService.alert('Нужно создать паспорт для добавления оффлайн комнат', 'Пойми!');
+            },
+          },
+        ],
+      })}
+    </>,
+  ];
+};
