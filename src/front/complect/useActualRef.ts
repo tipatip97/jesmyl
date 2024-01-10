d445fe36f316e15ad0f47b131226b7e36b7f82bf
PSlobodskoy <PSlobodskoy@one.kz>
2024-01-10 07:58:57 +0300

    fet(useActualRef): add own type

--- src/front/complect/useActualRef.ts
+++ src/front/complect/useActualRef.ts
@@ -3,7 +3,9 @@ import { useMemo } from 'react';
 const setter = () => ({});
 const emptyArr: [] = [];
 
-export const useActualRef = <Value>(value: Value): { readonly current: Value } => {
+export type ActualRef<Value> = { readonly current: Value };
+
+export const useActualRef = <Value>(value: Value): ActualRef<Value> => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const ref = useMemo(setter as never, emptyArr as never) as { readonly current: Value };
   (ref as any).current = value;
