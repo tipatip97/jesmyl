556d98a41fd3a9ad896f1b20429d42c99a0933bc
PSlobodskoy <PSlobodskoy@one.kz>
2024-01-30 14:10:32 +0300

    fix(ExpandableContent): add postfix

--- src/front/complect/expand/ExpandableContent.tsx
+++ src/front/complect/expand/ExpandableContent.tsx
@@ -1,26 +1,31 @@
 import { ReactNode, useState } from 'react';
 import EvaIcon, { EvaIconName } from '../eva-icon/EvaIcon';
+import mylib from '../my-lib/MyLib';
 
 interface Props {
-  children: ReactNode;
-  title: ReactNode;
+  children: ReactNode | ((isExpand: boolean) => ReactNode);
+  title: ReactNode | ((isExpand: boolean) => ReactNode);
+  postfix?: ReactNode | ((isExpand: boolean) => ReactNode);
   icon?: EvaIconName;
 }
 
-export const ExpandableContent = ({ children, title, icon }: Props) => {
+export const ExpandableContent = ({ children, title, icon, postfix }: Props) => {
   const [isExpand, setIsExpand] = useState(false);
 
   return (
     <>
-      <span
-        className="flex flex-gap flex-max pointer"
-        onClick={() => setIsExpand(!isExpand)}
-      >
-        {icon && <EvaIcon name={icon} />}
-        {title}
-        <EvaIcon name={isExpand ? 'chevron-up' : 'chevron-down'} />
-      </span>
-      {isExpand && children}
+      <div className="flex full-width between">
+        <span
+          className="flex flex-gap flex-max pointer"
+          onClick={() => setIsExpand(!isExpand)}
+        >
+          {icon && <EvaIcon name={icon} />}
+          {mylib.isFunc(title) ? title(isExpand) : title}
+          <EvaIcon name={isExpand ? 'chevron-up' : 'chevron-down'} />
+        </span>
+        {mylib.isFunc(postfix) ? postfix(isExpand) : postfix}
+      </div>
+      {isExpand && (mylib.isFunc(children) ? children(isExpand) : children)}
     </>
   );
 };
