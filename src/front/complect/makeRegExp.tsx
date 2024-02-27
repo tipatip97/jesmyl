141355382d7441bbecc900cd98228bf4d8393ab4
PSlobodskoy <PSlobodskoy@one.kz>
2024-02-27 13:24:45 +0300

    feat(utils): makeRegExp init

---
+++ src/front/complect/makeRegExp.tsx
@@ -0,0 +1,15 @@
+const regs: Record<string, RegExp> = {};
+type StrRegExp = `/${string}/${'d' | ''}${'g' | ''}${'i' | ''}${'m' | ''}${'s' | ''}${'u' | ''}${'y' | ''}`;
+
+export function makeRegExp(reg: StrRegExp, isResetLastIndex?: boolean) {
+  if (regs[reg] === undefined)
+    try {
+      regs[reg] = new RegExp(reg.slice(1, reg.lastIndexOf('/')), reg.slice(reg.lastIndexOf('/') + 1));
+    } catch (e) {
+      throw Error(`Incorrect arg passed in ${makeRegExp.name}(${reg})`);
+    }
+
+  if (isResetLastIndex === true) regs[reg].lastIndex = 0;
+
+  return regs[reg];
+}
