ce0385b3c3f8538d82d33f7ff5520bfb935739fd
PSlobodskoy <PSlobodskoy@one.kz>
2024-05-31 13:00:07 +0300

    tg gul94i bot init

---
+++ src/back/sides/telegram-bot/gul94i/gul94i-bot.ts
@@ -0,0 +1,18 @@
+import { jesmylTgBot } from '../bot';
+import { tglogger } from '../log/log-bot';
+import { JesmylTelegramBot } from '../tg-bot';
+
+export const gul94iTelegramBot = new JesmylTelegramBot({
+  bot: jesmylTgBot,
+  chatId: -1001862285194,
+  logger: tglogger,
+});
+
+gul94iTelegramBot.onChatMessages((bot, message) => {
+  if (message.text?.toLowerCase() === 'матюки') {
+    bot.deleteMessage(message.message_id, message.chat.id);
+  }
+
+  if (message.text?.startsWith('/start') && message.from) {
+  }
+});
