d4fe8088c40de44a18d34c03f88117b0f7cdccc9
PSlobodskoy <PSlobodskoy@one.kz>
2024-07-05 15:29:20 +0300

    feat(): visits tg inform

--- src/back/complect/soki/parts/60-Visits.ts
+++ src/back/complect/soki/parts/60-Visits.ts
@@ -1,3 +1,4 @@
+import { tglogger } from '../../../sides/telegram-bot/log/log-bot';
 import { SokiServerExecs } from './50-Execs';
 
 export class SokiServerVisits extends SokiServerExecs {
@@ -8,6 +9,23 @@ export class SokiServerVisits extends SokiServerExecs {
 
     if (this.lastVisit !== date.toLocaleDateString()) {
       this.statistic.pastVisits[this.lastVisit] = this.statistic.visits.length;
+
+      tglogger.log(
+        `Посещения за ${this.lastVisit}` +
+          ` (${this.statistic.visits.length})\n\n<blockquote expandable>` +
+          this.statistic.visits
+            .map(
+              visit =>
+                `<b>${visit.fio ?? '???'}</b>` +
+                (visit.tgId ? ` @${visit.nick || '-'}` : '') +
+                ` ${visit.version}` +
+                ` ${visit.deviceId}` +
+                ` ${visit.time}`,
+            )
+            .join('\n') +
+          `</blockquote>`,
+      );
+
       this.statistic.visits = [];
       this.lastVisit = date.toLocaleDateString();
     }
