6e91269844d81cc792024537cc7383e9c6174b87
PSlobodskoy <PSlobodskoy@one.kz>
2024-07-05 15:24:56 +0300

    fix(script.mjs): code for bible text compiling

--- script.mjs
+++ script.mjs
@@ -1,31 +1,80 @@
 import fs from 'fs';
+// export * from './src/back/apps/bible/kas';
 
-const dir = `./src/back/apps/cm/+case/`;
+// const dir = `./src/back/apps/cm/+case/`;
 
-const cols = JSON.parse('' + fs.readFileSync(`${dir}cols.json`));
+// const cols = JSON.parse('' + fs.readFileSync(`${dir}cols.json`));
 
-const refs = {};
+// fs.writeFileSync(`${dir}cols1.json`, JSON.stringify(cols, null, '  '));
 
-cols.coms.forEach(com => {
-  if (com.r == null) return;
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
 
-  Object.entries(com.r).forEach(([key, val]) => {
-    if (!val) return;
-    if (refs[key] == null) refs[key] = {};
+const chapters = [];
 
-    refs[key][com.w] = val;
-  });
+const bookPath = './src/back/apps/bible/';
 
-  delete com.r;
+fs.readdirSync(bookPath + '+nrt').forEach(name => {
+  if (!name.endsWith('htm')) return;
+
+  const content = '' + fs.readFileSync(`${bookPath}+nrt/${name}`);
+
+  chapters[parseInt(name) - 1] = content
+    .replace(/<\/?HTML>|<\/?BODY>/g, '')
+    .replace(/ - /g, ' — ')
+    .replace(/<\/?p\/?>\d* ?/g, '')
+    .replace(/ , .|([\n\s])["„]/g, '$1«')
+    .replace(/„/g, '«')
+    .replace(/"/g, '»')
+    .replace(/<font color=/g, '<span class=')
+    .replace(/<(\/?)font/g, '<$1span')
+    .trim()
+    .split(/<h4>.+?<\/h4>/)
+    .filter((_, i) => i)
+    .map(chapter =>
+      chapter
+        .trim()
+        .split(/[\r\n]+/)
+        .map(v => v.trim()),
+    );
 });
 
-cols.cats.forEach(cat => {
-  if (cat.k !== 'dict' || refs[cat.w] == null) return;
+fs.writeFileSync(`${bookPath}nrt.json`, JSON.stringify({ chapters }, null, '  '));
 
-  cat.d = refs[cat.w];
-  delete refs[cat.w];
-});
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 
-console.log(refs);
+// const chapters = Array.from({ length: 40 }, () => null);
 
-fs.writeFileSync(`${dir}cols1.json`, JSON.stringify(cols, null, '  '));
+// const bookPath = './src/back/apps/bible/';
+
+// fs.readdirSync(bookPath + '+kas').forEach(name => {
+//   if (!name.endsWith('HTM')) return;
+
+//   const content = '' + fs.readFileSync(`${bookPath}/${name}`);
+
+//   chapters[parseInt(name) - 1] = content
+//     .replace(/<\/?HTML>|<\/?BODY>/g, '')
+//     .replace(/<\/?p>\d* ?/g, '')
+//     .replace(/ , .|([\n\s])["„]/g, '$1«')
+//     .replace(/„/g, '«')
+//     .replace(/"/g, '»')
+//     .replace(/ - /g, ' — ')
+//     .trim()
+//     .replace(/<Стих 14 прежних.+?>/g, ' ')
+//     .split(/<h4>.+?<\/h4>/)
+//     .filter((_, i) => i)
+//     .map(chapter => chapter.trim().split(/[\r\n]+/));
+// });
+
+// fs.writeFileSync(`${bookPath}kas.json`, JSON.stringify({ chapters }, null, '  '));
