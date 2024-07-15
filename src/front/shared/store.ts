31d9012fc6a9e193f28082994f45dc1177e328f4
PSlobodskoy <PSlobodskoy@one.kz>
2024-07-15 11:50:14 +0300

    fix(tuner): reducer removed

--- src/front/shared/store.ts
+++ src/front/shared/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { gamerReducer } from '../components/apps/gamer/Gamer.store';
-import { tunerReducer } from '../components/apps/tuner/Tuner.store';
 
 export const store = configureStore({
   reducer: {
-    tuner: tunerReducer,
     gamer: gamerReducer,
   },
   middleware: mw => mw({ serializableCheck: false }),
