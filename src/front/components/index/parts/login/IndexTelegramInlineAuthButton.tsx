944c3a2c23358e9afb234f1b9b6869e767525299
PSlobodskoy <PSlobodskoy@one.kz>
2024-08-12 08:30:25 +0300

    fix(IndexTelegramInlineAuthButton): button has tg-auth icon-button

--- src/front/components/index/parts/login/IndexTelegramInlineAuthButton.tsx
+++ src/front/components/index/parts/login/IndexTelegramInlineAuthButton.tsx
@@ -1,8 +1,8 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import BrutalItem from '../../../../complect/brutal-item/BrutalItem';
 import mylib from '../../../../complect/my-lib/MyLib';
 import { TelegramWebAppApiOr } from '../../../../complect/tg-app/getTgApi';
-import { TheIconLoading } from '../../../../complect/the-icon/IconLoading';
+import IconButton from '../../../../complect/the-icon/IconButton';
 import { IconAuthorizedStrokeRounded } from '../../../../complect/the-icon/icons/authorized';
 import { IconTelegramStrokeRounded } from '../../../../complect/the-icon/icons/telegram';
 import { soki } from '../../../../soki';
@@ -10,30 +10,10 @@ import { removePullRequisites, useSetAuth } from '../../molecules';
 
 export const IndexTelegramInlineAuthButton = () => {
   const setAuth = useSetAuth();
-  const navigate = useNavigate();
 
   return (
     <TelegramWebAppApiOr>
-      {(api, isLoading) => {
-        if (api?.initData) {
-          return (
-            <BrutalItem
-              icon={<IconTelegramStrokeRounded />}
-              title="Авторизоваться"
-              onClick={() => {
-                soki.send({ tgNativeAuthorization: api.initDataUnsafe.user }, 'index').on(({ tgAuthorization }) => {
-                  if (!tgAuthorization || !tgAuthorization.ok || mylib.isStr(tgAuthorization.value)) return;
-
-                  setAuth(tgAuthorization.value);
-                  removePullRequisites();
-                  soki.onConnect();
-                  navigate('..');
-                });
-              }}
-            />
-          );
-        }
-
+      {api => {
         return (
           <Link
             to="login"
@@ -42,7 +22,27 @@ export const IndexTelegramInlineAuthButton = () => {
             <BrutalItem
               icon={<IconAuthorizedStrokeRounded />}
               title="Авторизоваться"
-              box={<TheIconLoading isLoading={isLoading} />}
+              box={
+                api?.initData && (
+                  <IconButton
+                    Icon={IconTelegramStrokeRounded}
+                    className="color--ok"
+                    onClick={event => {
+                      event.preventDefault();
+
+                      soki
+                        .send({ tgNativeAuthorization: api.initDataUnsafe.user }, 'index')
+                        .on(({ tgAuthorization }) => {
+                          if (!tgAuthorization || !tgAuthorization.ok || mylib.isStr(tgAuthorization.value)) return;
+
+                          setAuth(tgAuthorization.value);
+                          removePullRequisites();
+                          soki.onConnect();
+                        });
+                    }}
+                  />
+                )
+              }
             />
           </Link>
         );
