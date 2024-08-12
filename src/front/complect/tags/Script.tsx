d19f5410fecedefa1119021f53a26e4c2df4da41
PSlobodskoy <PSlobodskoy@one.kz>
2024-08-12 08:14:20 +0300

    feat(tags): add in complect

---
+++ src/front/complect/tags/Script.tsx
@@ -0,0 +1,29 @@
+import { DOMAttributes, useEffect } from 'react';
+import mylib from '../my-lib/MyLib';
+import { useActualRef } from '../useActualRef';
+
+type Props = DOMAttributes<any> & {
+  src: string;
+  async?: boolean;
+};
+
+export const Script = ({ src, onLoad, ...props }: Props) => {
+  const propRef = useActualRef(props);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+    const props = propRef.current;
+
+    mylib.keys(props).forEach(key => {
+      if (mylib.isBool(props[key])) script[key as never] = props[key] as never;
+      else if (mylib.isFunc(props[key])) script[key.toLowerCase() as never] = props[key] as never;
+      else if (mylib.isStr(props[key])) script.setAttribute(key, props[key] as never);
+    });
+
+    return () => script.remove();
+  }, [onLoad, propRef, src]);
+
+  return <></>;
+};
