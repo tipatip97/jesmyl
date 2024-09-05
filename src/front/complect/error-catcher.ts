30f32fd1cc5e203cabd1005381ce820ae32dd9ea
PSlobodskoy <PSlobodskoy@one.kz>
2024-09-05 17:26:39 +0300

    fix(): logFrontErrors fixed

--- src/front/complect/error-catcher.ts
+++ src/front/complect/error-catcher.ts
@@ -3,6 +3,10 @@ export const logFrontErrors = () => {
 
   if (container == null) return;
 
+  const errorList = document.createElement('div');
+  let timeout: TimeOut;
+  let isInserted = false;
+
   window.onerror = function myErrorHandler(errorMessage, url, lineNumber, _, error) {
     const div = document.createElement('div');
 
@@ -11,7 +15,16 @@ export const logFrontErrors = () => {
     div.style.color = 'red';
     div.style.marginBottom = '20px';
 
-    container.appendChild(div);
+    errorList.appendChild(div);
+
+    if (!isInserted) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        const root = document.querySelector('#root');
+
+        if (!root?.innerHTML) container.appendChild(errorList);
+      }, 1000);
+    }
 
     return false;
   };
