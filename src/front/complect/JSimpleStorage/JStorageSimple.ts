f86fa9d65a6391b324faeb8be7bc66ce7f7e2292
PSlobodskoy <PSlobodskoy@one.kz>
2024-11-11 13:07:12 +0300

    feat(): simple values create SetOrArray

--- src/front/complect/JSimpleStorage/JStorageSimple.ts
+++ src/front/complect/JSimpleStorage/JStorageSimple.ts
@@ -2,15 +2,22 @@ import Eventer, { EventerValueCallback, EventerValueListeners } from '../../../b
 
 const keysSet = new Set();
 
-export class JStorageSimpleVal<Value = boolean | number | string> {
+type simpleValue = boolean | number | string;
+
+type AnyValue = simpleValue | simpleValue[] | Record<string | number, simpleValue>;
+export type AnySimpleValue = AnyValue | AnyValue[] | Record<string | number, AnyValue>;
+
+export class JStorageSimpleVal<Value> {
   protected key: string;
-  protected defaultValue: Value;
   protected setter: (val: Value) => void;
   protected getter: (isRetDef: boolean) => Value | null;
   protected listeners: EventerValueListeners<Value> = [];
 
-  constructor(storageKey: string, id: string, defaultValue: Value) {
-    this.defaultValue = defaultValue;
+  constructor(
+    storageKey: string,
+    id: string,
+    protected defaultValue: Value,
+  ) {
     this.key = `simpleValue:${storageKey}:${id}`;
 
     if (keysSet.has(this.key)) throw Error(`Обнаружены одинаковые ключи для JStorageSimpleVal ${this.key}`);
---
+++ src/front/complect/JSimpleStorage/exports/SetOrArray.ts
@@ -0,0 +1,35 @@
+import mylib from '../../my-lib/MyLib';
+import { AnySimpleValue, JStorageSimpleVal } from '../JStorageSimple';
+
+export class JStorageSetOrArrayVal<
+  Value extends AnySimpleValue[] | Set<AnySimpleValue>,
+> extends JStorageSimpleVal<Value> {
+  private value: Value | und;
+
+  constructor(storageName: string, id: string, defaultValue: Value) {
+    super(storageName, id, defaultValue);
+
+    const key = this.key;
+
+    this.setter = val => {
+      this.value = val as never;
+      localStorage[key] = JSON.stringify(Array.from(val));
+    };
+
+    this.getter = (isRetDef: boolean) => {
+      if (this.value !== undefined) return this.value;
+      const lsValue = localStorage[key];
+
+      if (lsValue === undefined) return isRetDef ? defaultValue : null;
+
+      const value = (this.value =
+        defaultValue instanceof Set
+          ? new Set(JSON.parse(lsValue))
+          : mylib.isArr(defaultValue)
+            ? JSON.parse(lsValue)
+            : defaultValue);
+
+      return value;
+    };
+  }
+}
