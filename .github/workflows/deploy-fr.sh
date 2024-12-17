3fe57d0fe3c396811fa00497b764e814edf4194d
tipatip <aik442557@gmail.com>
2024-12-17 16:23:47 +0300

    deploy

---
+++ .github/workflows/deploy-fr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Stop on error
+set -e
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# Move to folder with the apps from the github
+cd /root
+# Clone the repo
+git clone <your-git-repo-link>
+# Move to specific repo
+cd /root/<name-of-repo>
+# Install Node.js
+nvm install
+# Check node version
+node -v
+# Check npm version
+npm -v
+# Install dependencies
+npm ci
+# Build application
+npm build
+# Move build to the www folder
+cp -rf /root/<your-project-name>/dist/* /var/www/default
+# Remove repo
+rm -rf /root/<your-project-name>
